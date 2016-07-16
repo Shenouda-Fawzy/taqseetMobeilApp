6306e2eeefc754d9159b28c56be87b0a38dc527c
Shenouda-Fawzy <Shenouda.Fawzy@hotmail.com>
2016-07-16 14:57:00 +0300

    Customer insertion and display all customer added

---
+++ app/src/main/java/shenouda_fawzy/taqseet/CustomerDetailActivity.java
@@ -0,0 +1,13 @@
+package shenouda_fawzy.taqseet;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CustomerDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_detail);
+    }
+}
